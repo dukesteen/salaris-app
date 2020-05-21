af40caf63f637a6209f308a91671e4ee7cb8c3ae
Dukeeeee <40759437+GithubDuke@users.noreply.github.com>
2020-05-21 23:54:46 +0200

    Added salary_model.dart and salary_bloc.dart

---
+++ lib/blocs/salary_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:salaris_app/models/salary_model.dart';
+
+class SalaryBloc extends ChangeNotifier {
+  List<SalaryModel> _salaryEntries = [
+    SalaryModel(
+      hoursWorked: 10.0,
+      dateWorked: DateTime.now(),
+      hourlyWage: 5.0,
+    )
+  ];
+
+  List<SalaryModel> get salaryEntries => _salaryEntries;
+
+  add(SalaryModel entry) {
+    _salaryEntries.add(entry);
+    notifyListeners();
+  }
+
+  remove(SalaryModel entry) {
+    _salaryEntries.remove(entry);
+    notifyListeners();
+  }
+}
---
+++ lib/models/salary_model.dart
@@ -0,0 +1,7 @@
+class SalaryModel {
+  final double hoursWorked;
+  final DateTime dateWorked;
+  final double hourlyWage;
+
+  SalaryModel({this.hoursWorked, this.dateWorked, this.hourlyWage});
+}
